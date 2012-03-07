bb7c0940dad287a7181ea0d784b44e02b71ee2d3
John Anderson <sontek@gmail.com>
2012-03-07 01:54:38 -0600

    base real-time chat implemented

---
+++ chatter2/chatter2/views.py
@@ -0,0 +1,21 @@
+def index(request):
+    """ Base view to load our template """
+    return {}
+
+def socketio_service(request):
+    """ The view that will launch the socketio listener """
+
+    # gevent-socketio puts this into the environment
+    socketio = request.environ["socketio"]
+
+    # keep trying to get messages from the websocket
+    while True:
+        message = socketio.receive()
+        if message:
+            if message["type"] == "event":
+                if message['name'] == "chat":
+                    # we got a new chat event from the client, send it out to
+                    # all the listeners
+                    socketio.broadcast_event('chat', ''.join(message['args']))
+    return {}
+
