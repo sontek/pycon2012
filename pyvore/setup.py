2c27588ffe4281a3782ffc41838d03b6dd55c9c5
Whit Morriss <whit@surveymonkey.com>
2012-03-13 18:52:05 -0700

    read in reqs
    mild formatting

--- pyvore/setup.py
+++ pyvore/setup.py
@@ -1,13 +1,13 @@
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = [
-]
+requires = open('requirements.txt').split('\n')
 
 setup(name='pyvore',
       version='0.0',
@@ -19,7 +19,7 @@ setup(name='pyvore',
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
-      author='',
+      author='sontek/abourget',
       author_email='',
       url='',
       keywords='web wsgi bfg pylons pyramid',
